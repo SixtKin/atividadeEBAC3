58e0f7643c820a924d8c735c40a7a77e6ceff7cf
Kin <k.c.nobre@hotmail.com>
2023-10-23 13:40:38 -0300

    Comitando atividade

---
+++ atividade.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('comparador');
+
+function compararCampo () {
+    const numeroA = document.getElementById('numeroA').value
+    const numeroB = document.getElementById('numeroB').value
+    const alert = document.getElementById('alert')
+
+    if (numeroA >= numeroB){
+        document.querySelector('.error-message').classList.remove('hidden')
+        document.querySelector('.success-message').classList.add('hidden')
+    } else {
+        document.querySelector('.success-message').classList.remove('hidden')
+        document.querySelector('.error-message').classList.add('hidden')
+    }
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    compararCampo()
+})
+
